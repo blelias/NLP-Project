f985529925c09c20c5a35ae90971526475cfdc5c
Elias Bjørne-larsen <bl.elias96@gmail.com>
2021-05-16 00:17:04 +0200

    Add song list dataset, modifying soup.find()

--- scraper.py
+++ scraper.py
@@ -3,15 +3,23 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import re
-import numpy as np
 # %%
 song_list = []
 def get_song(soup):
     song_dict = {}
+
     # Get the text values from website
-    title = soup.find("h1", class_="SongHeader__Title-sc-1b7aqpg-7 eJWiuG").get_text()
-    album = soup.find("div", class_="HeaderTracklist__Album-sc-1qmk74v-3 hxXYDz").get_text()
-    lyrics = soup.find("div", class_="SongPageGrid-sc-1vi6xda-0 DGVcp Lyrics__Root-sc-1ynbvzw-0 kkHBOZ").get_text(separator="<br/>")
+    #title = soup.find("h1", class_="SongHeader__Title-sc-1b7aqpg-7 eJWiuG").get_text()
+    #album = soup.find("div", class_="HeaderTracklist__Album-sc-1qmk74v-3 hxXYDz").get_text()
+    #lyrics = soup.find("div", class_="SongPageGrid-sc-1vi6xda-0 DGVcp Lyrics__Root-sc-1ynbvzw-0 kkHBOZ").get_text(separator="<br/>")
+    title = soup.find("h1").get_text()
+    print(title)
+    album = soup.find("div", class_=re.compile(r".*?_Album-.*?")).get_text()
+    print(album)
+    lyrics = soup.find("div", class_=re.compile(r".*?Lyrics__Container.*?")).get_text(seperator="<br/>")
+    print(lyrics)
+    
+    # HOLDER PÅ MED: få soup.find til å funke (gjør slik at den targeter hele klassenavnet med regex)
 
     # Split text based on bracket location
     split_text = re.split(r"\[.*?]", lyrics)
@@ -35,12 +43,12 @@ def load_url(url_list):
     for url in url_list:
         req = requests.get(url) # Load the webpage
         soup = BeautifulSoup(req.content, 'html.parser')
+        print(req.status_code)
         get_song(soup)
 # %%
-#url_list = ["https://genius.com/John-lennon-imagine-lyrics", "https://genius.com/The-beatles-help-lyrics", "https://genius.com/The-beatles-yellow-submarine-lyrics"]
-url_list = ["https://genius.com/John-lennon-imagine-lyrics", "https://genius.com/The-beatles-help-lyrics"]
+url_list = ["https://genius.com/John-lennon-imagine-lyrics", "https://genius.com/The-beatles-help-lyrics", "https://genius.com/The-beatles-yellow-submarine-lyrics"]
+#url_list = ["https://genius.com/The-beatles-yellow-submarine-lyrics"]
 song_list = []
-
 load_url(url_list)
 # %%
 song_list
