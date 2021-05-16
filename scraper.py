69721a69e8be17c7697fe0bb8643b3b4aa9c54d8
Elias Bjørne-larsen <bl.elias96@gmail.com>
2021-05-16 16:32:30 +0200

    Update

--- scraper.py
+++ scraper.py
@@ -9,17 +9,12 @@ df_features = pd.read_csv("song_features_dataset.csv")
 song_list = []
 def get_song(soup, title):
     song_dict = {}
-
-    # Get the text values from website
-    #title = soup.find("h1", class_="SongHeader__Title-sc-1b7aqpg-7 eJWiuG").get_text()
-    #album = soup.find("div", class_="HeaderTracklist__Album-sc-1qmk74v-3 hxXYDz").get_text()
-    #lyrics = soup.find("div", class_="SongPageGrid-sc-1vi6xda-0 DGVcp Lyrics__Root-sc-1ynbvzw-0 kkHBOZ").get_text(separator="<br/>")
-    #title = soup.find("h1").text.strip()
-    #if not any(title in dict for dict in song_list):
-    if not any(d['title'] == title for d in song_list):
+    # If the title does not exist in song_list
+    if not any(d['title'] == title for d in song_list): # Fix this one
         try:
             print(title)
-            album = soup.find("div", class_=re.compile(r'_Album-.*?"'[0:-1])).get_text()
+            #
+            # album = soup.find("div", class_=re.compile(r'_Album-.*?"'[0:-1])).get_text()
             lyrics = soup.find("div", class_=re.compile(r'"Lyrics__Container.*?"'[1:-1])).get_text(separator="<br/>") # .*?Lyrics__Container.*?
             print("Success")
         except:
@@ -40,10 +35,10 @@ def get_song(soup, title):
             type = re.sub(r"\s]", "]", type)
             song_dict[type] = text
         song_dict["title"] = title
-        song_dict["album"] = album
+        #song_dict["album"] = album
         song_list.append(song_dict)
     else:
-        print("Already there")
+        print("Error: This iteration should not have happend")
 # %%
 def load_url(n_songs): # Call get_song function
     url_list = []
@@ -52,46 +47,44 @@ def load_url(n_songs): # Call get_song function
         song_title = standard_title(song_title)       
         url_list.append(create_url(song_title))
     #print(url_list)
-    for i in range(20): # Choose number of iterations
+    for i in range(10): # Choose number of iterations
         for url in url_list:
+            # If the title from the URL does not exisit in song_list, continue
             if not any(standard_title(d['title']) == standard_title(re.findall(r"beatles-(.*)-lyrics", url)[0]) for d in song_list):
                 req = requests.get(url) # Load the webpage
                 soup = BeautifulSoup(req.content, 'html.parser')
+                if req.status_code == 404:
+                    url_list.remove(url) # url_list.remove(create_url(title))
+                    print("Could not find " + url)
+                    break
                 print(req.status_code)
                 title = soup.find("h1").text.strip()
                 get_song(soup, title)
-            else:  
-                try:
+
+                # If successful, remove url from url-list
+                if any(standard_title(d['title']) == standard_title(re.findall(r"beatles-(.*)-lyrics", url)[0]) for d in song_list): 
                     url_list.remove(url) # url_list.remove(create_url(title))
                     print("Removed " + url)
-                    break
-                except:
-                    print("Already removed")
-        #print(url_list)
 # %%
 def standard_title(title):
     title = title.replace(' ', '-')
     title = title.replace("'", '')
     title = title.replace("’", "")
+    title = title.replace("(", "")
+    title = title.replace(")", "")
     return title
 def create_url(title):
     url = "https://genius.com/The-beatles-" + standard_title(title) + "-lyrics"
     return url
 # %%
-song_list = []
-load_url(3) # Choose number of songs
+#song_list = []
+load_url(100) # Choose number of songs
+to_csv(song_list)
 # %%
-len(song_list)
-# %%
-any(standard_title(d['title']) == standard_title(re.findall(r"beatles-(.*)-lyrics", url)[0]) for d in song_list)
-# %%
-url_list = ['https://genius.com/The-beatles-12-Bar-Original-lyrics', 'https://genius.com/The-beatles-A-Day-in-the-Life-lyrics', 'https://genius.com/The-beatles-A-Hard-Days-Night-lyrics']
-# %%
-url = url_list[0]
-# %%
-song_list
-# %%
-re.findall(r"beatles-(.*)-lyrics", url)[0]
+# Function for adding song list to pandas dataframe
+def to_csv(song_list):
+    df = pd.DataFrame(song_list)
+    df.to_csv("lyrics_dataset.csv")
 # %%
 len(song_list)
 # %%
